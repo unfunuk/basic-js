b3c0fc85e8223f7c76ecba3950539968bb156b1a
unfunuk <a.s.s.t.e.r.i.x.0@gmail.com>
2021-03-15 15:19:55 +0300

    feat:fix

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,18 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-  let array = [];
+  if (members === null || members === undefined) {
+    return false;
+  }
   let k = 0;
+  let array = [];
   for (let i = 0; i < members.length; i++) {
     if (typeof members[i] == "string") {
       let mas = members[i].split('');
-      array[k] = mas[0].toUpperCase();
-      k++;
+      for (let s = 0; s < mas.length; s++) {
+        if (mas[s] != ' ') {
+          array[k] = mas[s].toUpperCase();
+          k++;
+          break;
+        }
+      }
     }
   }
-  if (array.length == 0) {
+  if (array.length === 0) {
     return false;
   }
-  alert(array.sort().join());
+  return array.sort().join('');
 
 };

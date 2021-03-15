b06a21496e378fa9bdf8b46c479785fe7a0eb65a
unfunuk <a.s.s.t.e.r.i.x.0@gmail.com>
2021-03-15 16:48:23 +0300

    feat: fix

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new RangeError('Error')
+  }
+  let mas = [];
+  let k = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] != "--discard-next" && arr[i] != "--discard-prev" && arr[i] != "--double-next" && arr[i] != "--double-prev") {
+      mas[k] = arr[i];
+      k++;
+    }
+    else {
+      if (arr[i] == "--discard-next" && arr[i + 1] != undefined) {
+
+        i++;
+
+      }
+      if (arr[i] == "--discard-prev" && arr[i - 1] != undefined && arr[i - 2] != "--discard-next") {
+        mas.splice(k - 1, 1);
+        k--;
+      }
+      if (arr[i] == "--double-next" && arr[i + 1] != undefined) {
+        mas[k] = arr[i + 1];
+        k++;
+      }
+      if (arr[i] == "--double-prev" && arr[i - 1] != undefined && arr[i - 2] != "--discard-next") {
+        mas[k] = arr[i - 1];
+        k++;
+      }
+    }
+  }
+  return mas;
 };
+
+

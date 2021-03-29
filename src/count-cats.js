0eed9909fe15aee2040eb919bd2860966fd4dfe0
unfunuk <a.s.s.t.e.r.i.x.0@gmail.com>
2021-03-29 21:33:12 +0300

    fix: solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -3,9 +3,9 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function countCats(matrix) {
   let count = 0;
   for (let i = 0; i < matrix.length; i++) {
-    let mas = matrix[i];
-    for (let s = 0; s < mas.length; s++) {
-      if (mas[s] == "^^") {
+    let arr = matrix[i];
+    for (let s = 0; s < arr.length; s++) {
+      if (arr[s] == "^^") {
         count++;
       }
     }
